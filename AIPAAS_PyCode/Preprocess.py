5651f62522ea62fe6a9a18fd9a62a9d17a11989b
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2019-10-11 16:56:07 -0400

    Changes

--- AIPAAS_PyCode/Preprocess.py
+++ AIPAAS_PyCode/Preprocess.py
@@ -11,8 +11,7 @@ Created on Tue Sep 17 12:19:06 2019
 """
 
 # =============================================================================
-# Warning !! Only to be used on training and validation data
-# Always use s_rep>1 to give non similar train_out values
+# # Always use s_rep>1 to give non similar train_out values
 # =============================================================================
 
 #Libraries
@@ -41,27 +40,27 @@ class cMAPSS:
         cls.s_len     = s_len 
     
     @classmethod
-    def preprocess(cls, train_data):
+    def preprocess(cls, input_data):
         
-        cls.no_engines  = train_data.iloc[-1,0]
-        cls.max_cycles  = train_data['Cycles'].max()
-        engine_id       = train_data.iloc[:,0]
-        train_data      = train_data.iloc[:,2:]
+        cls.no_engines  = input_data.iloc[-1,0]
+        cls.max_cycles  = input_data['Cycles'].max()
+        engine_id       = input_data.iloc[:,0]
+        input_data      = input_data.iloc[:,2:]
 
-        data_variance   = train_data.var()
-        train_data      = train_data.loc[:, data_variance > cls.threshold]
+        data_variance   = input_data.var()
+        input_data      = input_data.loc[:, data_variance > cls.threshold]
         
         for i in range(1,cls.no_engines+1):
             
-            train_data.loc[engine_id == i,:] = train_data.loc[engine_id == i,:].apply(cls.savgol)
+            input_data.loc[engine_id == i,:] = input_data.loc[engine_id == i,:].apply(cls.savgol)
                 
         #Normalising after data has been filtered
-        train_data = train_data.apply(lambda x: (x-x.mean())/(x.max()-x.min()))
+        input_data = input_data.apply(lambda x: (x-x.mean())/(x.max()-x.min()))
         
         #preparing data for the LSTM
         cls.train_in  = np.full((cls.no_engines*cls.s_rep, 
                                  cls.max_cycles, 
-                                 train_data.shape[1]),
+                                 input_data.shape[1]),
                                  1000.0)
             
         cls.train_out = np.full((cls.no_engines*cls.s_rep),1e-2)
@@ -69,8 +68,8 @@ class cMAPSS:
         for i in range(0, cls.no_engines*cls.s_rep, cls.s_rep):
             
             e_id      = i/cls.s_rep + 1
-            cycle_len = train_data.loc[engine_id == e_id, :].shape[0]
-            temp      = train_data.loc[engine_id == e_id, :]
+            cycle_len = input_data.loc[engine_id == e_id, :].shape[0]
+            temp      = input_data.loc[engine_id == e_id, :]
             temp      = temp.to_numpy()
             cls.train_in[i, :cycle_len, :] = temp
             
--- AIPAAS_PyCode/Testing.py
+++ AIPAAS_PyCode/Testing.py
@@ -16,8 +16,27 @@ Created on Tue Sep 17 12:19:06 2019
 # you can but only if you are zen
 # =============================================================================
 
+
+import numpy as np
+
+
+
 class cMAPPS:
     
-    def get_score(cls, model):
+    def get_score(cls, model, test_in, true_rul):
+        
+        cls.true_rul = true_rul.to_numpy()
+        cls.est_rul = model.predict(test_in, batch_size=None)  
         
         
+        #Calculating S score from the NASA paper, variables can be found there
+        
+        d = cls.est_rul - cls.true_rul
+        
+        d[d<0]  = np.exp(-(d[d<0]/10)) - 1
+        
+        d[d>=0] = np.exp(d[d>=0]/13) - 1
+        
+        cls.s = d.sum()
+        
+        print(f'The score is - {cls.s} !!! Cry or Celebrate')
