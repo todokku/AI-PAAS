24ed5c1bd4003c88b190583998a25523cdb86cbf
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2019-12-15 19:55:04 +0400

    c

--- CMAPSS/Script_Main.py
+++ CMAPSS/Script_Main.py
@@ -7,6 +7,7 @@ Created on Mon Dec  2 18:01:08 2019
 
 import Run
 
+
 prepros_params = {'win_len'   : 7, 
                   'p_order'   : 3, 
                   'std_fac'   : -0.25,    #Stagered Repetition
@@ -16,17 +17,17 @@ prepros_params = {'win_len'   : 7,
                   'denoising' : True}
     
 model_hparams = {'rnn_type'    : 'simpleRNN',
-                 'rnn_neurons' : [5,5],
-                 'ff_neurons'  : [5]}
+                 'rnn_neurons' : [10,10],
+                 'ff_neurons'  : [10]}
 
 train_hparams = {'dropout'        : 0.3,
                  'rec_dropout'    : 0.1,
-                 'l2_k'           : 0.005,
+                 'l2_k'           : 0.01,
                  'l2_b'           : 0.0,
                  'l2_r'           : 0.002,
                  'lr'             : 0.005,
                  'beta'           : [0.9,0.999],
-                 'val_split'      : 0.40,
+                 'val_split'      : 0.30,
                  'epochs'         : 5,
                  'batch_size'     : 128,
                  'epsilon'        : 1e-7,
@@ -44,13 +45,11 @@ train_params = {**model_hparams,
 Run.cMAPPS('CMAPSS',
            prepros_params,
            train_params,
-           1,
-           path = 'C:/Users/janar/Downloads/CMAPSSData/',
-           tracking = False)
+           1)
 
 
 
-
+# path = 'C:/Users/janar/Downloads/CMAPSSData/',
 
 
 
