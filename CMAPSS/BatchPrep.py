8cbacdcb495ebcca9ad341fb4b258f32521b220d
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2020-02-26 13:49:54 -0500

    c

--- CMAPSS/BatchPrep.py
+++ CMAPSS/BatchPrep.py
@@ -7,3 +7,20 @@ AIAS ,Ryerson Univesity
 
 """
 import numpy as np
+
+
+class PrepCNNInOut:
+
+    def __init__(self, s_len=5, initial_cutoff=0., ins_dropped=0.):
+        self.s_len = s_len
+        self.initial_cutoff = initial_cutoff
+        self.ins_dropped = ins_dropped
+        self.no_ins = None
+        self.cycle_len = None
+
+    def _prep_train_inputs(self, input_array, e_id):
+        train_seq = np.split(input_array, np.cumsum(np.unique(e_id, return_counts=True)[1])[:-1])
+        no_features = input_array.shape[1]
+        for i, seq in enumerate(train_seq):
+            train_seq[i] = seq.reshape(1, -1, no_features)
+        return train_seq
--- CMAPSS/SequencePrep.py
+++ CMAPSS/SequencePrep.py
@@ -71,10 +71,6 @@ class PrepRnnInOut_seq:
     def __init__(self):
         self.max_rul = None
 
-    def _assign_dummy(self, x):
-        x[x[0] + 1:] = 0.
-        return x
-
     def _prep_train_inputs(self, input_array, e_id):
         train_seq = np.split(input_array, np.cumsum(np.unique(e_id, return_counts=True)[1])[:-1])
         no_features = input_array.shape[1]
